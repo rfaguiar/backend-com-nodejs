9af8bef67ffa7d7f1fe0cb9e42094ac8eadfe388
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-02-23 18:41:27 -0300

    curso nodejs db

--- app/infra/ConnectionFactory.js
+++ app/infra/ConnectionFactory.js
@@ -19,10 +19,19 @@ function createDBConnection() {
             password: 'root',
             database: 'casadocodigo_nodejs_test'
         });
+    } else if (process.env.NODE_ENV == 'production') {
+        var urlDb = process.env.CLEARDB_DATABASE_URL;
+        var grupos = urlDb.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)?reconnect=true/);
+        return mysql.createConnection({
+            host:grupos[2],
+            user:grupos[0],
+            password:grupos[1],
+            database: grupos[3]
+        });
     }
 }
 
 module.exports = function () {
-    
+
     return createDBConnection;
 }
